fcf1667748fd10a1032402c5ce1c7db29cd30ed9
Youngho Kwon <127571276+dy-08@users.noreply.github.com>
2025-11-12 23:52:08 +0900

    wip(TodoCard): .

---
+++ src/components/TodoCard/TodoCard.jsx
@@ -0,0 +1,10 @@
+import { Fragment } from 'react';
+import styles from './TodoCard.module.css';
+
+export default function TodoCard() {
+  return (
+    <Fragment>
+      <div className={styles.card4x5}></div>
+    </Fragment>
+  );
+}
