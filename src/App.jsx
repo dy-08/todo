07a355d92cd893e3102d8625425725c83ed46326
Youngho Kwon <127571276+dy-08@users.noreply.github.com>
2025-11-13 17:24:54 +0900

    feat: todo 추가 기능 구현 (#3)

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
 import './App.css';
 import TodoCard from './components/TodoCard/TodoCard';
-import TodoHeader from './components/TodoHeader/TodoHeader';
-import TodoList from './components/TodoList/TodoList';
-import TodoSearch from './components/TodoSearch/TodoSearch';
 
 function App() {
-    return (
-        <TodoCard>
-            <TodoHeader />
-            <TodoList />
-            <TodoSearch />
-        </TodoCard>
-    );
+    return <TodoCard />;
 }
 
 export default App;
--- src/components/TodoCard/TodoCard.jsx
+++ src/components/TodoCard/TodoCard.jsx
@@ -1,10 +1,18 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import styles from './TodoCard.module.css';
+import TodoHeader from '../TodoHeader/TodoHeader';
+import TodoList from '../TodoList/TodoList';
+import TodoSearch from '../TodoSearch/TodoSearch';
 
-export default function TodoCard({ children }) {
+export default function TodoCard() {
+    const [todos, setTodos] = useState(initailTodos);
     return (
-        <Fragment>
-            <div className={styles.card4x5}>{children}</div>
-        </Fragment>
+        <div className={styles.card4x5}>
+            <TodoHeader />
+            <TodoList todos={todos} setTodos={setTodos} />
+            <TodoSearch setTodos={setTodos} />
+        </div>
     );
 }
+
+const initailTodos = ['React 강의 듣기', 'PostCSS 사용해보기', '학원가기'];
