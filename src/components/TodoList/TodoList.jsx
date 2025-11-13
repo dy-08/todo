785340b2457864d600303e2690b2b04546aac3e8
Youngho Kwon <dy-08@naver.com>
2025-11-13 17:21:14 +0900

    feat: todo 추가 기능 구현

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,15 +2,13 @@ import { useState } from 'react';
 import styles from './TodoList.module.css';
 import { AiOutlineDelete } from 'react-icons/ai';
 
-export default function TodoList() {
-    const [todos, setTodos] = useState(['React 강의 듣기', 'PostCSS 사용해보기', '학원가기']);
+export default function TodoList({ todos, setTodos }) {
     const handleDeleteTodo = (index) => {
         const idx = todos.findIndex((todo, i) => i === index);
         setTodos((prev) => {
             return prev.filter((todo, index) => index !== idx);
         });
     };
-    console.log('todos:', todos);
     return (
         <ul className={styles.todoList}>
             {todos.map((todo, index) => (
--- src/components/TodoSearch/TodoSearch.jsx
+++ src/components/TodoSearch/TodoSearch.jsx
@@ -1,13 +1,22 @@
 import styles from './TodoSearch.module.css';
+import { useState } from 'react';
 
-export default function TodoSearch() {
+export default function TodoSearch({ setTodos }) {
+    const [form, setForm] = useState('');
+    const handleChange = (e) => {
+        const { value } = e.target;
+        setForm(value);
+    };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setTodos((prev) => [...prev, form]);
+        setForm('');
+    };
     return (
-        <form className={styles.todoSearch}>
+        <form className={styles.todoSearch} onSubmit={handleSubmit}>
             <div className={styles.searchWrap}>
-                <input className={styles.input} type='text' />
-                <button className={styles.button} type='submit'>
-                    Add
-                </button>
+                <input className={styles.input} onChange={handleChange} value={form} type='text' />
+                <button className={styles.button}>Add</button>
             </div>
         </form>
     );
