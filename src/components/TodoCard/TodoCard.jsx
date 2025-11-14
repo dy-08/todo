7eb0f54938cf6c543e9327083c1ad91d0e492a29
Youngho Kwon <127571276+dy-08@users.noreply.github.com>
2025-11-14 17:07:03 +0900

    todo 필터 기능 구현, 완료된 항목 중앙선 스타일 추가, 로직 변경
    feat: 필터(all/active/completed) 기능 및 완료된 항목 중앙선 스타일 추가
    fix: index 기반 로직 제거로 체크 상태 꼬임 문제 해결

--- src/components/TodoCard/TodoCard.jsx
+++ src/components/TodoCard/TodoCard.jsx
@@ -1,18 +1,26 @@
-import { Fragment, useState } from 'react';
+import { useState } from 'react';
 import styles from './TodoCard.module.css';
 import TodoHeader from '../TodoHeader/TodoHeader';
 import TodoList from '../TodoList/TodoList';
 import TodoSearch from '../TodoSearch/TodoSearch';
 
 export default function TodoCard() {
-    const [todos, setTodos] = useState(initailTodos);
-    return (
-        <div className={styles.card4x5}>
-            <TodoHeader />
-            <TodoList todos={todos} setTodos={setTodos} />
-            <TodoSearch setTodos={setTodos} />
-        </div>
-    );
+  const [todos, setTodos] = useState(initailTodos);
+  const [checked, setChecked] = useState([]);
+  const [state, setState] = useState('all');
+  return (
+    <div className={styles.card4x5}>
+      <TodoHeader state={state} setState={setState} />
+      <TodoList
+        todos={todos}
+        setTodos={setTodos}
+        setChecked={setChecked}
+        checked={checked}
+        state={state}
+      />
+      <TodoSearch setTodos={setTodos} />
+    </div>
+  );
 }
 
 const initailTodos = ['React 강의 듣기', 'PostCSS 사용해보기', '학원가기'];
--- src/components/TodoSearch/TodoSearch.jsx
+++ src/components/TodoSearch/TodoSearch.jsx
@@ -2,22 +2,29 @@ import styles from './TodoSearch.module.css';
 import { useState } from 'react';
 
 export default function TodoSearch({ setTodos }) {
-    const [form, setForm] = useState('');
-    const handleChange = (e) => {
-        const { value } = e.target;
-        setForm(value);
-    };
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setTodos((prev) => [...prev, form]);
-        setForm('');
-    };
-    return (
-        <form className={styles.todoSearch} onSubmit={handleSubmit}>
-            <div className={styles.searchWrap}>
-                <input className={styles.input} onChange={handleChange} value={form} type='text' />
-                <button className={styles.button}>Add</button>
-            </div>
-        </form>
-    );
+  const [form, setForm] = useState('');
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setForm(value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (form) {
+      setTodos((prev) => [...prev, form]);
+      setForm('');
+    }
+  };
+  return (
+    <form className={styles.todoSearch} onSubmit={handleSubmit}>
+      <div className={styles.searchWrap}>
+        <input
+          className={styles.input}
+          onChange={handleChange}
+          value={form}
+          type='text'
+        />
+        <button className={styles.button}>Add</button>
+      </div>
+    </form>
+  );
 }
